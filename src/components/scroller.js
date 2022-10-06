0c4e8fec5fbfd4d76e67c5a8a564ef71c434bbac
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-10-06 19:44:06 +0900

    update

--- src/components/scroller.js
+++ src/components/scroller.js
@@ -30,14 +30,14 @@ export const Scroller = ({gesture, isLoaded}) => {
                 calculator();
                 setTransition('left 0ms');
             } else if (gesture === "left" && !stop){
-                setTransition('left 300ms ease-in-out');
+                setTransition('left 500ms');
                 setStop(false);
-                let add = position + 15;
+                let add = position + 10;
                 setPosition(add);
             } else if (gesture === "right" && !stop){
-                setTransition('left 300ms ease-in-out');
+                setTransition('left 500ms');
                 setStop(false);
-                let add = position - 15;
+                let add = position - 10;
                 setPosition(add);
             }
         }
